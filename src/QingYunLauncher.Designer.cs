1ba92bd0027a617dae45932a00865b8e8d4daab5
LYErikuy <111875719+Buelie@users.noreply.github.com>
2023-04-01 12:55:58 +0800

    C#
    C#

---
+++ src/QingYunLauncher.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace QingYunLauncher
+{
+    partial class QingYunLauncher
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QingYunLauncher));
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.下载游戏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.下载游戏ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.其他版本ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.下载模组ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.下载模组APIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forgeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fabricToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.更多ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.下载资源包光影ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.运行环境ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.运行参数ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.调试ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.启动器帮助LToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏帮助GToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minecraft官网社区ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.启动器更新日志LToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏更新日志ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.下载游戏ToolStripMenuItem,
+            this.游戏ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // 下载游戏ToolStripMenuItem
+            // 
+            this.下载游戏ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.下载游戏ToolStripMenuItem1,
+            this.下载模组ToolStripMenuItem,
+            this.下载模组APIToolStripMenuItem,
+            this.下载资源包光影ToolStripMenuItem});
+            this.下载游戏ToolStripMenuItem.Name = "下载游戏ToolStripMenuItem";
+            resources.ApplyResources(this.下载游戏ToolStripMenuItem, "下载游戏ToolStripMenuItem");
+            // 
+            // 下载游戏ToolStripMenuItem1
+            // 
+            this.下载游戏ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.其他版本ToolStripMenuItem});
+            this.下载游戏ToolStripMenuItem1.Name = "下载游戏ToolStripMenuItem1";
+            resources.ApplyResources(this.下载游戏ToolStripMenuItem1, "下载游戏ToolStripMenuItem1");
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            resources.ApplyResources(this.toolStripMenuItem2, "toolStripMenuItem2");
+            // 
+            // 其他版本ToolStripMenuItem
+            // 
+            this.其他版本ToolStripMenuItem.Name = "其他版本ToolStripMenuItem";
+            resources.ApplyResources(this.其他版本ToolStripMenuItem, "其他版本ToolStripMenuItem");
+            // 
+            // 下载模组ToolStripMenuItem
+            // 
+            this.下载模组ToolStripMenuItem.Name = "下载模组ToolStripMenuItem";
+            resources.ApplyResources(this.下载模组ToolStripMenuItem, "下载模组ToolStripMenuItem");
+            // 
+            // 下载模组APIToolStripMenuItem
+            // 
+            this.下载模组APIToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.forgeToolStripMenuItem,
+            this.fabricToolStripMenuItem,
+            this.更多ToolStripMenuItem});
+            this.下载模组APIToolStripMenuItem.Name = "下载模组APIToolStripMenuItem";
+            resources.ApplyResources(this.下载模组APIToolStripMenuItem, "下载模组APIToolStripMenuItem");
+            // 
+            // forgeToolStripMenuItem
+            // 
+            this.forgeToolStripMenuItem.Name = "forgeToolStripMenuItem";
+            resources.ApplyResources(this.forgeToolStripMenuItem, "forgeToolStripMenuItem");
+            // 
+            // fabricToolStripMenuItem
+            // 
+            this.fabricToolStripMenuItem.Name = "fabricToolStripMenuItem";
+            resources.ApplyResources(this.fabricToolStripMenuItem, "fabricToolStripMenuItem");
+            // 
+            // 更多ToolStripMenuItem
+            // 
+            this.更多ToolStripMenuItem.Name = "更多ToolStripMenuItem";
+            resources.ApplyResources(this.更多ToolStripMenuItem, "更多ToolStripMenuItem");
+            // 
+            // 下载资源包光影ToolStripMenuItem
+            // 
+            this.下载资源包光影ToolStripMenuItem.Name = "下载资源包光影ToolStripMenuItem";
+            resources.ApplyResources(this.下载资源包光影ToolStripMenuItem, "下载资源包光影ToolStripMenuItem");
+            // 
+            // 游戏ToolStripMenuItem
+            // 
+            this.游戏ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.运行环境ToolStripMenuItem,
+            this.运行参数ToolStripMenuItem,
+            this.调试ToolStripMenuItem});
+            this.游戏ToolStripMenuItem.Name = "游戏ToolStripMenuItem";
+            resources.ApplyResources(this.游戏ToolStripMenuItem, "游戏ToolStripMenuItem");
+            // 
+            // 运行环境ToolStripMenuItem
+            // 
+            this.运行环境ToolStripMenuItem.Name = "运行环境ToolStripMenuItem";
+            resources.ApplyResources(this.运行环境ToolStripMenuItem, "运行环境ToolStripMenuItem");
+            // 
+            // 运行参数ToolStripMenuItem
+            // 
+            this.运行参数ToolStripMenuItem.Name = "运行参数ToolStripMenuItem";
+            resources.ApplyResources(this.运行参数ToolStripMenuItem, "运行参数ToolStripMenuItem");
+            // 
+            // 调试ToolStripMenuItem
+            // 
+            this.调试ToolStripMenuItem.Name = "调试ToolStripMenuItem";
+            resources.ApplyResources(this.调试ToolStripMenuItem, "调试ToolStripMenuItem");
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.启动器帮助LToolStripMenuItem,
+            this.游戏帮助GToolStripMenuItem,
+            this.minecraft官网社区ToolStripMenuItem,
+            this.启动器更新日志LToolStripMenuItem,
+            this.游戏更新日志ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            resources.ApplyResources(this.帮助ToolStripMenuItem, "帮助ToolStripMenuItem");
+            // 
+            // 启动器帮助LToolStripMenuItem
+            // 
+            this.启动器帮助LToolStripMenuItem.Name = "启动器帮助LToolStripMenuItem";
+            resources.ApplyResources(this.启动器帮助LToolStripMenuItem, "启动器帮助LToolStripMenuItem");
+            // 
+            // 游戏帮助GToolStripMenuItem
+            // 
+            this.游戏帮助GToolStripMenuItem.Name = "游戏帮助GToolStripMenuItem";
+            resources.ApplyResources(this.游戏帮助GToolStripMenuItem, "游戏帮助GToolStripMenuItem");
+            // 
+            // minecraft官网社区ToolStripMenuItem
+            // 
+            this.minecraft官网社区ToolStripMenuItem.Name = "minecraft官网社区ToolStripMenuItem";
+            resources.ApplyResources(this.minecraft官网社区ToolStripMenuItem, "minecraft官网社区ToolStripMenuItem");
+            this.minecraft官网社区ToolStripMenuItem.Click += new System.EventHandler(this.minecraft官网社区ToolStripMenuItem_Click);
+            // 
+            // 启动器更新日志LToolStripMenuItem
+            // 
+            this.启动器更新日志LToolStripMenuItem.Name = "启动器更新日志LToolStripMenuItem";
+            resources.ApplyResources(this.启动器更新日志LToolStripMenuItem, "启动器更新日志LToolStripMenuItem");
+            // 
+            // 游戏更新日志ToolStripMenuItem
+            // 
+            this.游戏更新日志ToolStripMenuItem.Name = "游戏更新日志ToolStripMenuItem";
+            resources.ApplyResources(this.游戏更新日志ToolStripMenuItem, "游戏更新日志ToolStripMenuItem");
+            // 
+            // QingYunLauncher
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "QingYunLauncher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 下载游戏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 下载游戏ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 下载模组ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 下载模组APIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 下载资源包光影ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 其他版本ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forgeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fabricToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 更多ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 运行环境ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 运行参数ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 调试ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 启动器帮助LToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏帮助GToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minecraft官网社区ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 启动器更新日志LToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏更新日志ToolStripMenuItem;
+    }
+}
+
